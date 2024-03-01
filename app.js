0704428b4606ec07d0f043b30f472297493ea8b9
AbrarAhmed1990 <appdev1990w@gmail.com>
2024-02-29 17:50:42 -0800

    JS if else Statement

---
+++ app.js
@@ -0,0 +1,23 @@
+// <------ Chapter NO 9 ------>
+// Q no: 1
+
+// var city = "Karachi";
+// if (prompt("Enter your city name") === city){alert("Welcome to city of light")}
+// else if(prompt !== city){alert("Thanks for your visit")}
+// else (prompt !== city);{};
+
+// Q no: 2
+
+// var genderMale = "Male" , genderFemale = "Female" ;
+// if (prompt("Enter your Gender") === genderMale){alert("Good Morning Sir.")}
+// else if(prompt !== genderMale){alert("Good Morning Ma'am.")}
+// else (prompt !== genderFemale,genderMale);{};
+
+// Q no: 3
+
+// var r = "Red" , y = "Yellow" , g = "Green" ;
+
+// if (prompt("Enter traffic light color") === r){alert("Must Stop")}
+// else if (prompt === y) {alert("Ready to Move")}
+// else if (prompt === g) {alert("Move Now")}
+// else (prompt !== r,y,g); {};
